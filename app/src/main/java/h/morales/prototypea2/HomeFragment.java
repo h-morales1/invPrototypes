a3d9890f4758023d21bf2b1d1f642f9739ecfd70
h-morales1 <hmorales5@tutanota.com>
2023-05-20 16:56:28 -0500

    got sort options menu working for home gallery

--- app/src/main/java/h/morales/prototypea2/HomeFragment.java
+++ app/src/main/java/h/morales/prototypea2/HomeFragment.java
@@ -1,16 +1,24 @@
 package h.morales.prototypea2;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -67,6 +75,7 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -75,6 +84,32 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    //
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_by:
+                Toast.makeText(getContext(), "clicked sort by", Toast.LENGTH_LONG).show();
+                break;
+        }
+        //return super.onOptionsItemSelected(item);
+        return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        //FragmentManager fragmentManager = getParentFragmentManager();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        Fragment frag = fragmentManager.findFragmentById(R.id.frame_layout);
+        Log.d(TAG, "onCreateOptionsMenu: NAME:: " + frag.getId());
+        MenuInflater menuInflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.top_options_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    //
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/h/morales/prototypea2/MainActivity.java
+++ app/src/main/java/h/morales/prototypea2/MainActivity.java
@@ -1,11 +1,21 @@
 package h.morales.prototypea2;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 import h.morales.prototypea2.databinding.ActivityMainBinding;
 
@@ -42,6 +52,31 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
+
+    // handle on click for top options menu SORTING
+    /*
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_by:
+                Toast.makeText(this, "clicked sort by", Toast.LENGTH_LONG).show();
+                break;
+        }
+        //return super.onOptionsItemSelected(item);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // TODO: remove it if you have provlems
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment frag = fragmentManager.findFragmentById(R.id.frame_layout);
+        Log.d(TAG, "onCreateOptionsMenu: NAME:: " + frag.getId());
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_options_menu, menu);
+        return true;
+    }
+    */
 
     // method to replace frame layout with particular fragment
     private void replaceFragment(Fragment fragment) {
