ba1d5bd78dcb73829fe7f7bd02882f117e65bf78
h-morales1 <hmorales5@tutanota.com>
2023-05-23 16:40:30 -0500

    got picture taking functionality working in add_item fragment

--- app/src/main/java/h/morales/prototypea2/MainActivity.java
+++ app/src/main/java/h/morales/prototypea2/MainActivity.java
@@ -2,19 +2,38 @@ package h.morales.prototypea2;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.zip.Inflater;
 
 import h.morales.prototypea2.databinding.ActivityMainBinding;
@@ -22,6 +41,9 @@ import h.morales.prototypea2.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    ActivityResultLauncher<Uri> takePictureLauncher;
+    private static final int CAMERA_PERM_CODE = 1;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
         //show home fragment by default
         replaceFragment(new HomeFragment());
+        imageUri = createUri();
+        registerPictureLauncher();
 
 
         // do something when a user selects one of the items on the bottom
@@ -84,5 +108,66 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    /*private File createImageFile() throws IOException {
+        //create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        //save a file: path for use with ACTION_VIEW intents
+        String currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }*/
+
+    private Uri createUri() {
+        File imageFile = new File(getApplicationContext().getFilesDir(), "camera_photo.jpg");
+        return FileProvider.getUriForFile(getApplicationContext(), "com.h.morales.fileprovider", imageFile);
+    }
+
+    private void checkCameraPermsAndOpenCam() {
+        //
+        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //request perms
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            takePictureLauncher.launch(imageUri);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CAMERA_PERM_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                takePictureLauncher.launch(imageUri);
+            } else {
+               //
+            }
+        }
+    }
+
+    private void registerPictureLauncher() {
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.TakePicture(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                try {
+                    if(result) {
+                        //testing
+                        Log.d(TAG, "onActivityResult: PIC TEST");
+                    }
+                } catch (Exception e) {
+                    //
+                }
+            }
+        });
+    }
+
+
+    public void takePhoto(View view) {
+        checkCameraPermsAndOpenCam();
+        Log.d(TAG, "takePhoto: TEST");
     }
 }
